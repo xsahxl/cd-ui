3c18fa8f3309dbce7c44e47c269a69c324bc4685
xsahxl <xsahxl@126.com>
2023-04-13 11:26:01 +0800

    version: @serverless-cd/credential-ui@0.0.5
    Signed-off-by: xsahxl <xsahxl@126.com>

--- dumidoc.js
+++ dumidoc.js
@@ -59,7 +59,7 @@ module.exports = [
   },
   {
     package: '@serverless-cd/credential-ui',
-    version: '0.0.4',
+    version: '0.0.5',
     path: './packages/credential-ui',
     name: 'credential',
   },
